abdb1914958b51adbe97675ebcc8d0f8fcd3a24e
Hauvert Pacheco <hpachec1@gmail.com>
2022-12-29 12:45:11 -0800

    commit

---
+++ Dojo Weather/script.js
@@ -0,0 +1,19 @@
+
+
+function cityweatheralertburbank(element){
+    alert("You are switching weather to Burbank");
+}
+
+function cityweatheralertchicago(element){
+    alert("You are switching weather to Chicago");
+}
+
+function cityweatheralertdallas(element){
+    alert("You are switching weather to Dallas");
+}
+
+var cookiealertbox = document.querySelector("#cookiealert");
+
+function accept(){
+    cookiealertbox.remove();
+}
